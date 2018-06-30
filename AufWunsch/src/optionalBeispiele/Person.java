7c47af7d43fe4272b1934819d7b30a49148af922
TKSE1967 <TKSE1967@NLWE1967.opitz-consulting.int>
2018-06-30 06:43:55 +0200

    Optional Grundlagen

---
+++ AufWunsch/src/optionalBeispiele/Person.java
@@ -0,0 +1,20 @@
+package optionalBeispiele;
+
+import java.util.Optional;
+
+public class Person {
+
+	String name;
+	Optional<Integer> vielleichtGewicht;
+	
+	public Person(String name, Optional<Integer> vielleichtGewicht) {
+		this.name = name;
+		this.vielleichtGewicht = vielleichtGewicht;
+	}
+	public String getName() {
+		return name;
+	}
+	public Optional<Integer> getVielleichtGewicht() {
+		return vielleichtGewicht;
+	}
+}
