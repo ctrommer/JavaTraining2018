bd76770940954ce943c2b281f46b437ba7ae10b5
TKSE1967 <TKSE1967@NLWE1967.opitz-consulting.int>
2018-07-03 11:53:01 +0200

    Lösung aus Übung entfernt

--- MethodenReferenz/src/methodenReferenz/BestimmtesObjektInstanzmethode.java
+++ MethodenReferenz/src/methodenReferenz/BestimmtesObjektInstanzmethode.java
@@ -13,24 +13,10 @@ public class BestimmtesObjektInstanzmethode {
 
 	public static void schauspielerVerabschiedetAlleMitarbeiterMitConsumerUndLambda() {
 
-		Schauspieler arnold = new Schauspieler("I'll be back");
-
-		List<Mitarbeiter> mitarbeiterListe = Arrays.asList(new Mitarbeiter( "Karl"), new Mitarbeiter( "Hans"), new Mitarbeiter( "Egon"));
-
-		Consumer<Mitarbeiter> arnoldVerabschiedetMitarbeiter = mitarbeiter -> arnold.verabschieden(mitarbeiter);
-
-		mitarbeiterListe.forEach(arnoldVerabschiedetMitarbeiter);		
 	}
 
 	public static void schauspielerVerabschiedetAlleMitarbeiterMitConsumerUndMethodReferenz() {
 
-		Schauspieler arnold = new Schauspieler("I'll be back");
-
-		List<Mitarbeiter> mitarbeiterListe = Arrays.asList(new Mitarbeiter( "Karl"), new Mitarbeiter( "Hans"), new Mitarbeiter( "Egon"));
-
-		Consumer<Mitarbeiter> arnoldVerabschiedetMitarbeiter = arnold::verabschieden;
-		
-		mitarbeiterListe.forEach(arnoldVerabschiedetMitarbeiter);		
 	}	
 	
 }
