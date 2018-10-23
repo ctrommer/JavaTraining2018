f38940ffb79449c09a40e3f289ee0d08085f49cd
CurtWork <CurtWork@work.it>
2018-10-23 08:21:46 +0200

    jetzt neu mit Generics ( schon bereinigt )

---
+++ Generics/src/schleife/ArrayAusgeben.java
@@ -0,0 +1,18 @@
+package schleife;
+
+public class ArrayAusgeben {
+	public static void main( String[] args ) {
+		arrayMitForSchleifeAusgeben();
+		arrayMitForInAusgeben();		
+	}
+
+	/**
+	 * Erzeuge primitives Array von int und gebe es mit einer for Schleife aus.
+	 */
+	private static void arrayMitForSchleifeAusgeben() {		
+	}	
+	
+	private static void arrayMitForInAusgeben() {		
+	}
+	
+}
---
+++ Generics/src/schleife/CollectionIterieren.java
@@ -0,0 +1,39 @@
+package schleife;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class CollectionIterieren {
+	static Collection<String> namenCollection = new HashSet<>(); // genauso: new ArrayList<>()
+	
+	public static void main( String[] args ) {
+		namenCollection.add("Uwe");
+		namenCollection.add("Karl");
+		namenCollection.add("Uta");
+		
+		System.out.println("iterator: ");
+		collectionMitIteratorAusgeben();
+		
+		System.out.println("for in: ");
+		collectionMitForInAusgeben();
+		
+		System.out.println("while: ");
+		collectionMitWhileAusgeben();
+		
+		System.out.println("for each: ");
+		collectionMitForEachAusgeben();
+	}
+
+	private static void collectionMitWhileAusgeben( ) {		
+	}
+	
+	private static void collectionMitIteratorAusgeben() {
+	}
+
+	private static void collectionMitForInAusgeben() {
+	}
+
+	private static void collectionMitForEachAusgeben() {		
+	}	
+}
