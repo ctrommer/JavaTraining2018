c4658ca69d480227e89e0c2887abfa2eda73222f
CurtWork <CurtWork@work.it>
2018-10-26 09:25:48 +0200

    Grundlagen jetzt mit Kommentaren ( als Hilfe für Übungen )

--- Grundlagen/src/callBack/StringHelfer.java
+++ Grundlagen/src/callBack/StringHelfer.java
@@ -4,12 +4,10 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class StringHelfer {
+	
 	public void textLaengeZumQuadratUndDamitAufrufen( String text, Consumer<Double> textAusgeber ) {
-
-		double textLaengeZumQuadrat = Math.pow(text.length(), 2);
-		
-		textAusgeber.accept(textLaengeZumQuadrat);
-		
+		double textLaengeZumQuadrat = Math.pow(text.length(), 2);		
+		textAusgeber.accept(textLaengeZumQuadrat);		
 	}
 
 }
--- Grundlagen/src/syntax/AeussereKlasseMitInnererKlasse.java
+++ Grundlagen/src/syntax/AeussereKlasseMitInnererKlasse.java
@@ -5,46 +5,50 @@ public class AeussereKlasseMitInnererKlasse {
 	private int aeussereZahl = 1;
 	private static int aeussereStaticZahl = 2;
 
+	/**
+	 * Innere Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
+	 */
 	public class InnereKlasse {
 		int summeDerErreichbarenZahlen() {
 			return (aeussereZahl + aeussereStaticZahl); 
 		}		
 	}
 	
+	/**
+	 * Nested Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
+	 */
 	public static class NestedKlasse {
-		private int nestedZahl = 3;
-		private static int nestedStaticZahl = 4;
-
 		public int summeDerErreichbarenZahlen() {
-			return (aeussereStaticZahl + nestedZahl + nestedStaticZahl); 
+			return (aeussereStaticZahl); 
 		}
 	}
 
+	/**
+	 * Lokale innere Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
+	 */
 	void methodeMitLokalerInnereKlasse() {
 		final class LokaleInnereKlasse {
-			private int lokaleInnereZahl = 5;
-
-			public int getLokaleInnereZahl() {
-				return lokaleInnereZahl;
+			public int summeDerErreichbarenZahlen() {
+				return (aeussereZahl + aeussereStaticZahl); 
 			}
-
 		}
 		
 		LokaleInnereKlasse lokaleInnereKlasse = new LokaleInnereKlasse();
-		System.out.println(lokaleInnereKlasse.getLokaleInnereZahl());
+		System.out.println(lokaleInnereKlasse.summeDerErreichbarenZahlen());
 	}
 	
 	public static void main(String[] args) {
-		
+	
+		// innere Klasse
 		AeussereKlasseMitInnererKlasse aeussereKlasse = new AeussereKlasseMitInnererKlasse();
 		AeussereKlasseMitInnererKlasse.InnereKlasse innereKlasse = aeussereKlasse.new InnereKlasse();
 		
 		System.out.println(innereKlasse.summeDerErreichbarenZahlen());
-		
-		aeussereKlasse.methodeMitLokalerInnereKlasse();
-		
+				
+		// nested Klasse
 		AeussereKlasseMitInnererKlasse.NestedKlasse nested = new AeussereKlasseMitInnererKlasse.NestedKlasse();		
 		System.out.println(nested.summeDerErreichbarenZahlen());
 		
+		aeussereKlasse.methodeMitLokalerInnereKlasse();
 	}	
 }
