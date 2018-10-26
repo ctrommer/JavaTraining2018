cad0ff7ad2d712b5214db7a7eb2cece91da2e563
CurtWork <CurtWork@work.it>
2018-10-26 15:07:44 +0200

    Neu: Mocken

---
+++ Mocken/src/uebersetzen/TestUebersetzer.java
@@ -0,0 +1,35 @@
+package uebersetzen;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * 
+ * 1. Mockito downloaden: google | mockito download jar -> http://www.java2s.com/Code/Jar/m/Downloadmockitoall195jar.htm
+ * 		Achtung: hatte vorher das mockito genommen, was ich schon hatte. Leider bekam ich dadurch eine ClassNotFoundException.
+ * 
+ * 2. Rechte Maus auf Projekt | Build Path | Configure Build Path | 
+ * 		Libraries | Add external jar ... | mocktio auswählen
+ * 
+ */
+public class TestUebersetzer {
+	@Test
+	public void testeUebersetzer() {
+
+		// mockt die Klasse Uebersetzer
+		Uebersetzer mockUebersetzer = Mockito.mock(Uebersetzer.class);
+		
+		// der Mock übersetzt "Welt" mit "World"
+		Mockito.when(mockUebersetzer.uebersetze("Welt")).thenReturn("World");
+		
+		// erzeugt die Uebersetzerschnittstelle mit dem Mock
+		UebersetzerSchnittstelle zuTesten = new UebersetzerSchnittstelle();
+		
+		// testet die Uebersetzerschnittstelle mit dem Mock
+		String uebersetzung = zuTesten.uebersetze(mockUebersetzer, "Welt");
+		
+		// prueft, ob das Ergebnis korrekt ist
+		Assert.assertTrue(uebersetzung.equals("World"));
+	}
+}
---
+++ Mocken/src/uebersetzen/UebersetzerSchnittstelle.java
@@ -0,0 +1,11 @@
+package uebersetzen;
+
+/**
+ * Schnittstelle, die getestet werden soll.
+ *
+ */
+public class UebersetzerSchnittstelle {
+	String uebersetze( Uebersetzer uebersetzer, String zuUebersetzen ) {
+		return uebersetzer.uebersetze(zuUebersetzen);
+	}
+}
