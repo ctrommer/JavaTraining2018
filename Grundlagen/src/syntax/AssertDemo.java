38a94add6c2bcdd90872c7f13fde022b5d58340a
u92767 <u92767@NB-2502.opitz-consulting.int>
2019-01-10 07:46:50 +0100

    Kommentar erweitert, wie man Assertions aktiviert.

--- Grundlagen/src/syntax/AssertDemo.java
+++ Grundlagen/src/syntax/AssertDemo.java
@@ -7,6 +7,9 @@ public class AssertDemo {
 		// Für die Anwendung aktivieren mit
 		// Debug | Debug Configuration | Java Application | Assertions | Arguments | 
 		// VM Arguments -ea eintragen | Apply 
+		//
+		// Die Application muss einmal gelaufen sein, damit unter
+		// Java Application überhaupt Assertions erscheinen.
 		assert false: "Hallo";
 	
 		assert false;
