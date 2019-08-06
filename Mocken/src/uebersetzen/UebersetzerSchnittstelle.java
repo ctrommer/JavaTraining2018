ad4f4f1b8f06c4de53419d736dd1dcb8a5c71345
u92767 <u92767@NB-2502.opitz-consulting.int>
2019-08-06 09:59:29 +0200

    Mockito mit Annotationen nach Tim

--- Mocken/src/uebersetzen/UebersetzerSchnittstelle.java
+++ Mocken/src/uebersetzen/UebersetzerSchnittstelle.java
@@ -1,11 +1,19 @@
 package uebersetzen;
 
+import java.util.List;
+
 /**
  * Schnittstelle, die getestet werden soll.
  *
  */
 public class UebersetzerSchnittstelle {
+	
 	String uebersetze( Uebersetzer uebersetzer, String zuUebersetzen ) {
 		return uebersetzer.uebersetze(zuUebersetzen);
 	}
+	
+	List<String> uebersetzeMitMehrerenErgebnissen( Uebersetzer uebersetzer, String zuUebersetzen ) {
+		return uebersetzer.uebersetzeMitMehrerenErgebnissen(zuUebersetzen);
+	}
+	
 }
