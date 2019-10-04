9e05455de382393de8815fdcc5530e0811c0983f
Curt <Curt@LAPTOP-QUV5SHD3>
2019-10-04 16:25:54 +0200

    nested class ist jetzt static inner class
    Conflicts:
    	Grundlagen/src/syntax/AeussereKlasseMitInnererKlasse.java

--- Grundlagen/src/syntax/AeussereKlasseMitInnererKlasse.java
+++ Grundlagen/src/syntax/AeussereKlasseMitInnererKlasse.java
@@ -1,7 +1,7 @@
 package syntax;
 
 public class AeussereKlasseMitInnererKlasse {
-	
+
 	private int aeussereZahl = 1;
 	private static int aeussereStaticZahl = 2;
 
@@ -9,22 +9,20 @@ public class AeussereKlasseMitInnererKlasse {
 	 * Innere Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
 	 */
 
-	
 	/**
-	 * Nested Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
+	 * Statische innere Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
 	 */
-
 
 	/**
 	 * Lokale innere Klasse, die zeigt, auf welche Elemente der äusseren Klasse sie zugreifen darf.
 	 */
 
-	
 	public static void main(String[] args) {
 	
 		// innere Klasse
 				
-		// nested Klasse
+
+		// statische innere Klasse
 
 	}	
 }
