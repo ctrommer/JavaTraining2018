992161021eee0615ad7c1b349ddb9129e1554678
Curt <Curt@LAPTOP-QUV5SHD3>
2019-10-06 08:14:17 +0200

    Duration

--- Grundlagen/src/datumUndZeit/LocalDateTimeBeispiel.java
+++ Grundlagen/src/datumUndZeit/LocalDateTimeBeispiel.java
@@ -1,5 +1,6 @@
 package datumUndZeit;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -8,11 +9,22 @@ public class LocalDateTimeBeispiel {
 	public static void main(String[] args) {		
 		gebeAktuelleZeitAus();		
 		gebeAktuellesDatumInDreiMonatenAus();
+		gebeDauerVonEinerMinuteInSekundenAus();
 	}
 
 	private static void gebeAktuelleZeitAus() {
 	}
 	
 	private static void gebeAktuellesDatumInDreiMonatenAus() {
+	}
+
+	/**
+	 * Erzeuege Dauer von einer Minute.
+	 * Gebe die Dauer in Sekunen aus.
+	 */
+	private static void gebeDauerVonEinerMinuteInSekundenAus() {
+		Duration duration = Duration.ofMinutes(1);
+		System.out.println(duration.getSeconds());
+		
 	}
 }
