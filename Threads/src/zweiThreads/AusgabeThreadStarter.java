0700bf098703bc6ef035af074091ef8f7efe86ac
Curt <Curt@LAPTOP-QUV5SHD3>
2019-10-06 08:39:23 +0200

    Runnable als Functional Interface

--- Threads/src/zweiThreads/AusgabeThreadStarter.java
+++ Threads/src/zweiThreads/AusgabeThreadStarter.java
@@ -4,6 +4,11 @@ package zweiThreads;
 public class AusgabeThreadStarter {
 
 	public static void main(String[] args) {
+		ohneLambda();
+		mitLambda();
+	}
+	
+	public static void ohneLambda() {
 		// zwei Console-Ausgeber erzeugen
 		Runnable ersterAusgabeThreadStarter = new  ConsoleAusgeber("XXX erster Thread XXX", 1000);
 		Runnable zweiterAusgabeThreadStarter = new ConsoleAusgeber("OOO zweiter Thread OOO", 2000);
@@ -14,7 +19,22 @@ public class AusgabeThreadStarter {
 		
 		// die beiden Threads starten
 		ersterThread.start();
-		zweiterThread.start();
+		zweiterThread.start();		
+	}
+	
+	public static void mitLambda() {
+		// Lambda, dass Hallo ausgibt für Thread erzeugen
+		Runnable runnable = ()->gebeHalloInEigenemThreadAus();
+		
+		// Thread mit dem Lambda initialisieren
+		Thread thread = new Thread(runnable);
+		
+		// Threads starten
+		thread.start();
+	}
+	
+	public static void gebeHalloInEigenemThreadAus() {
+		System.out.println("Hallo");
 	}
 
 }
