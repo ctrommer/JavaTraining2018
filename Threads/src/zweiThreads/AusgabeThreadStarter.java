46326241a6dc4862cb1232902a60e9e08e5c3387
Curt <Curt@LAPTOP-QUV5SHD3>
2019-10-06 08:39:23 +0200

    Runnable als Functional Interface
    Conflicts:
    	Threads/src/zweiThreads/AusgabeThreadStarter.java

--- Threads/src/zweiThreads/AusgabeThreadStarter.java
+++ Threads/src/zweiThreads/AusgabeThreadStarter.java
@@ -4,11 +4,28 @@ package zweiThreads;
 public class AusgabeThreadStarter {
 
 	public static void main(String[] args) {
+		ohneLambda();
+		mitLambda();
+	}
+	
+	public static void ohneLambda() {
 		// zwei Console-Ausgeber erzeugen
 		
 		// 2 Threads mit den Console-Ausgeben initialisieren
 		
 		// die beiden Threads starten
+	}
+	
+	public static void mitLambda() {
+		// Lambda, dass Hallo ausgibt für Thread erzeugen
+		
+		// Thread mit dem Lambda initialisieren
+		
+		// Threads starten
+	}
+	
+	public static void gebeHalloInEigenemThreadAus() {
+		System.out.println("Hallo");
 	}
 
 }
