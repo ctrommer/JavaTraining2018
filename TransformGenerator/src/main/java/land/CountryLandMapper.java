aaa63bf47316cc75ca6dab76d51704b69e278cd1
ctrommer <Curt@LAPTOP-QUV5SHD3>
2020-01-04 08:22:14 +0100

    Uebung von Lösung bereinigt.

--- TransformGenerator/src/main/java/land/CountryLandMapper.java
+++ TransformGenerator/src/main/java/land/CountryLandMapper.java
@@ -1,42 +1,17 @@
 package land;
 
-import java.util.Collection;
-import java.util.List;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
-@Mapper
+// Als Hilfe: https://www.baeldung.com/mapstruct#Type
+// Defaultwert für die Einwohnerzahl
+// Datumsformat als: "dd-MM-yyyy HH:mm:ss"
+// Testen mit:
+// Rechte Maus auf zu testendes Projekt | Maven | Update Project...
+// Rechte Maus auf Projekt | Run as | Maven clean install
+// Die Unittests ausführen.
+
 public interface CountryLandMapper {
-	
-    @Mappings({
-        @Mapping(target="einwohnerzahl", source="country.population", defaultExpression = "java(0)"),	// population wird auf einwohnerzahl gemappt, 0 ist der default
-        @Mapping( target = "hauptstadt", source = "country.capital"),									// beachte: Stadt ist eigene Klasse, die auch wieder eine Methode braucht, siehe ( 1 )
-        @Mapping(target="letzterFussballWMTietel", source = "country.lastSoccerWorldChampionshipTitle", dateFormat = "dd-MM-yyyy HH:mm:ss"),
-        @Mapping(target= "staedte", source = "country.cities")											// beachte: Liste braucht eigene Methode, die wiederum funktioniert, weil es eine Methode für Stadt gibt       
-      })	
-	Land countryZuLand( Country country );																// name gibt es in land und country, wird deshalb  automatisch gemappt
-    
-    @Mappings( {
-    	@Mapping( target="population", source="land.einwohnerzahl" ),
-    	@Mapping( target = "capital", source = "land.hauptstadt" ),    	
-    	@Mapping( target="lastSoccerWorldChampionshipTitle", source = "land.letzterFussballWMTietel", dateFormat = "dd-MM-yyyy HH:mm:ss" ),
-    	@Mapping( target = "cities", source = "land.staedte" ) 
-    })
-	Country landZuCountry( Land land );
-
-    @Mappings({
-    	@Mapping( target = "hoehe", source = "city.elevation")
-    })
-    Stadt cityZuStadt( City city );																		// ( 1 )
-    
-    public abstract List<Stadt> citiesZuStadte( Collection<City> cities ); 
-    
-    @Mappings({
-    	@Mapping( target = "elevation", source = "stadt.hoehe")
-    })    
-    City stadtZuCity( Stadt stadt );
-    public abstract List<City> stadteZuCities( Collection<Stadt> staedte );
-
+	Land countryZuLand(Country spain);
+	Country landZuCountry(Land spanien);	
 }
+
--- TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
+++ TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
@@ -1,63 +1,14 @@
 package schiff;
 
-import java.time.LocalDateTime;
+// Methoden einfügen, die den Start der Konvertierung und das Ende der Konvertierung als LocalDateTime für Quelle und Ziel setzen.
 
-import org.mapstruct.AfterMapping;
-import org.mapstruct.BeforeMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-
-@Mapper
 public abstract class ShipSchiffMapper {
-	
-	@BeforeMapping
-	protected void startKonvertierungZuShipSetzen( Schiff schiff, @MappingTarget Ship ship ) {
-		LocalDateTime jetzt = LocalDateTime.now();
-		schiff.setStartKonvertierung(jetzt);
-		ship.setStartConversion(jetzt);
-	}
-	
-	@BeforeMapping 
-	protected void startKonvertierungZuSchiffSetzen( Ship ship, @MappingTarget Schiff schiff ) {
-		LocalDateTime jetzt = LocalDateTime.now();
-		schiff.setStartKonvertierung(jetzt);
-		ship.setStartConversion(jetzt);
-	}
-	
-	/**
-	 * Wenn man eine Extrawurst braten möchte, kann man die Mapping Methode auch selber schreiben.
-	 * Hier wird zum Beispiel das britische Pfund in kg umgerechnet.
-	 * @param ship
-	 * ship soll zu Schiff konvertiert werden.
-	 * @return
-	 * Schiff entstanden aus konvertierem ship.
-	 */
-	public Schiff shipZuSchiff( Ship ship ) {
-		if ( ship == null ) {
-			return null;
-		}
-		return new Schiff(ship.getName(), ship.getColour(), ship.getWeightInLBS()/2.205);
+
+	public Schiff shipZuSchiff(Ship ship) {
+		return null;
 	}
 
-	public Ship schiffZuShip( Schiff schiff ) {
-		if ( schiff == null ) {
-			return null;
-		}
-		return new Ship(schiff.getName(), schiff.getFarbe(), schiff.getGewichtInKg()*2.205);
+	public Ship schiffZuShip(Schiff schiff) {
+		return null;
 	}
-
-	@AfterMapping
-	protected void endeKonvertierungZuShipSetzen( Schiff schiff, @MappingTarget Ship ship ) {
-		LocalDateTime jetzt = LocalDateTime.now();
-		schiff.setEndeKonvertierung(jetzt);
-		ship.setEndConversion(jetzt);
-	}	
-	
-	@AfterMapping
-	protected void endeKonvertierungZuSchiffSetzen( Ship ship, @MappingTarget Schiff schiff ) {	
-		LocalDateTime jetzt = LocalDateTime.now();
-		schiff.setEndeKonvertierung(jetzt);
-		ship.setEndConversion(jetzt);		
-	}
-	
 }
