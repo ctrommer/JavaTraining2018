eb6acebe32e952f65baf9ab174020ec821c6566e
ctrommer <Curt@LAPTOP-QUV5SHD3>
2020-06-14 11:51:45 +0200

    Datei lesen, deren Inhalt in temp Datei schreiben

--- Rules/src/regeln/TestTemporaryFolder.java
+++ Rules/src/regeln/TestTemporaryFolder.java
@@ -2,6 +2,12 @@ package regeln;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,7 +19,7 @@ public class TestTemporaryFolder {
 	 * Erzeugt temporären Ordner für Test.
 	 */
 	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	public TemporaryFolder temporaryFolder = new TemporaryFolder(new File("src\\regeln"));
 	
 	/**
 	 * Erzeuge Datei in temmporärem Ordner und gebe den Pfad aus.
@@ -21,8 +27,41 @@ public class TestTemporaryFolder {
 	@Test
 	public void testeTemporaryFolder() {
 		try {
-			File file = temporaryFolder.newFile("MyTest.txt");
-			System.out.println(file.getPath());
+			File zielDatei = temporaryFolder.newFile("tempZielDatei.txt");
+			String textInDatei = "Dieser Text wird in die temporaere Datei geschrieben";
+			Files.write(Paths.get(zielDatei.getAbsolutePath()), textInDatei.getBytes(StandardCharsets.UTF_8) );
+			System.out.println(zielDatei.getPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Lese die Datei quellDatei.xml ein als Stream von Strings.
+	 * 
+	 * Erzeuge eine temporäre Datei im gleichen Pfad, aber ohne TemporaryFolder
+	 *
+	 * Schreibe das, was aus der Datei quellDatei.xml ausgelesen wurde in die temporäre Datei 
+	 * 
+	 */
+	@Test
+	public void testeAlternativeZuTemporaryFolder() {
+		try {
+			// Datei einlesen
+			Path verzeichnisUndDateinameQuelldatei = Paths.get("src\\regeln\\quellDatei.xml");
+			Stream<String> zeilen = Files.lines(verzeichnisUndDateinameQuelldatei);
+
+			// Datei im gleichen Pfad erzeugen
+			File erzeugteDatei = new File("src\\regeln\\temp.xml");
+			// damit Datei am Ende wieder geloescht wird
+			erzeugteDatei.deleteOnExit();
+
+		    // in neue Datei schreiben
+			String verzeichnisUndDateinameZielDatei = erzeugteDatei.getAbsolutePath();
+			Files.write(Paths.get(verzeichnisUndDateinameZielDatei), zeilen.collect(Collectors.toList()), StandardCharsets.UTF_8);
+
+			zeilen.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
