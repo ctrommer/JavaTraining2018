8aeb206ff20249d6a17cbeac492ee8516a2d1f0a
ctrommer <Curt@LAPTOP-QUV5SHD3>
2020-09-21 14:02:42 +0200

    Spring Batch

---
+++ SpringBatch/src/main/java/batch/FeldZuMitarbeiterMapper.java
@@ -0,0 +1,22 @@
+package batch;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+import model.Mitarbeiter;
+
+public class FeldZuMitarbeiterMapper implements FieldSetMapper<Mitarbeiter> {
+
+	
+	@Override
+	public Mitarbeiter mapFieldSet(FieldSet fieldSet) throws BindException {
+
+		Mitarbeiter mitarbeiter = new Mitarbeiter();
+		mitarbeiter.setMitarbeiterId(fieldSet.readString(0));
+		mitarbeiter.setMitarbeiterName(fieldSet.readString(1));
+		mitarbeiter.setGewicht(fieldSet.readInt(2));
+		return mitarbeiter;
+	}
+
+}
