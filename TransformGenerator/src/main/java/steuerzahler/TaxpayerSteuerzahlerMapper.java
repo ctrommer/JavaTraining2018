f899f90186300fa24bc56fc142d87ff9234905f3
ctrommer <Curt@LAPTOP-QUV5SHD3>
2020-10-04 07:48:35 +0200

    Mapper von Loesung bereinigt

--- TransformGenerator/src/main/java/steuerzahler/TaxpayerSteuerzahlerMapper.java
+++ TransformGenerator/src/main/java/steuerzahler/TaxpayerSteuerzahlerMapper.java
@@ -20,56 +20,12 @@ import org.mapstruct.Mappings;
 @Mapper
 public abstract class TaxpayerSteuerzahlerMapper {
 
-    @Mappings({
-        @Mapping( target = "investition", source = "taxpayer"),
-      })	
 	public abstract Steuerzahler taxpayerZuSteuerzahler( Taxpayer taxpayer );
 
-    @Mappings( {
-    	@Mapping( target = "investmentAmount", source = "investition.investitionBetrag" ),
-    	@Mapping( target = "investmentCurrency", source = "investition.investitionWaehrung" ),
-    })
     public abstract Taxpayer steuerzahlerZuTaxpayer( Steuerzahler steuerzahler );
 
     public Long map(Long quelle) {
-    	return quelle;
+    	return null;
     }
-
-    public String map(Investition quelle, @MappingTarget String ziel ) {
-    	return quelle.getInvestitionWaehrung();
-    }
-
-    protected Investition taxpayerZuInvestition( final Taxpayer taxpayer ) {
-    	return new Investition(taxpayer.getInvestmentAmount(), taxpayer.getInvestmentCurrency());
-    }
-
-    @AfterMapping
-    protected void steuerzahlerZuTaxpayerNullBehandeln( @MappingTarget final Steuerzahler steuerzahler ) {
-    	if ( steuerzahler != null ) {
-    		Investition investition = steuerzahler.getInvestition();
-	    	if ( investition != null ) {
-	    		if ( investition.getInvestitionWaehrung() == null ) {
-	    			investition.setInvestitionBetrag(null);
-	    		}
-	    	}
-	    	if ( investition != null ) {
-	    		if ( investition.getInvestitionBetrag() == null ) {
-	    			investition.setInvestitionWaehrung(null);
-	    		}
-	    	}
-    	}
-    }
-    
-    @AfterMapping
-    protected void taxpayerZuInvestitionNullBehandeln( @MappingTarget final Taxpayer taxpayer ) {
-    	if ( taxpayer != null ) {
-    		if ( taxpayer.getInvestmentCurrency() == null ) {
-    			taxpayer.setInvestmentAmount(null);
-    		}
-    		if ( taxpayer.getInvestmentAmount() == null ) {
-    			taxpayer.setInvestmentCurrency(null);
-    		}
-    	}
-	}
     
 }
