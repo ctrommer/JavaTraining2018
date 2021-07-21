acd2dbc16099d8149757a9b7f5ff22d62d4144e1
ctrom <ctrom@LAPTOP-4F4UMOBG>
2021-07-21 09:50:53 +0200

    Umstellung auf Java 1.16 Anpassungen

--- SpringBatch/src/main/java/model/Mitarbeiter.java
+++ SpringBatch/src/main/java/model/Mitarbeiter.java
@@ -1,6 +1,6 @@
 package model;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "mitarbeiter")
 public class Mitarbeiter {
