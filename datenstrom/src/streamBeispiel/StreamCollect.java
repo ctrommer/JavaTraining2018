92df30d69863867d7129b61604540f29a85f13cc
ctrom <ctrom@LAPTOP-4F4UMOBG>
2021-08-16 13:09:24 +0200

    Key und Value von Map vertauschen

--- datenstrom/src/streamBeispiel/StreamCollect.java
+++ datenstrom/src/streamBeispiel/StreamCollect.java
@@ -1,8 +1,12 @@
 package streamBeispiel;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,6 +25,12 @@ public class StreamCollect {
 		System.out.println();
 		
 		streamZuMapMitDoppeleintraegen();
+		System.out.println();
+		
+		keyUndValueVertauschenOhneDoppelEintraege();
+		System.out.println();
+		
+		keyUndValueVertauschenMitDoppelEintraegen();
 		System.out.println();
 	}
 
@@ -58,6 +68,32 @@ public class StreamCollect {
 	 */
 	private static void streamZuMapMitDoppeleintraegen() {		
 	}			
-	
-	
+
+	/**
+	 * Key und Value einer Map vertauschen ohne Doppeleinträge und Map ausgeben
+	 */
+	public static void keyUndValueVertauschenOhneDoppelEintraege() {
+
+		Map<String, Integer> textUndHaeufigkeit = new HashMap<>();
+
+		textUndHaeufigkeit.put("einmal", 1);
+		textUndHaeufigkeit.put("zweimal", 2);
+		textUndHaeufigkeit.put("dreimal", 3);
+
+	}
+
+	/**
+	 * Key und Value einer Map vertrauschen mit Doppeleinträgen und Map ausgeben
+	 */
+	public static void keyUndValueVertauschenMitDoppelEintraegen() {
+		
+		Map<String, Integer> textUndHaeufigkeit = new HashMap<>();
+		
+		textUndHaeufigkeit.put("einmal", 1);
+		textUndHaeufigkeit.put("zweimal", 2);
+		textUndHaeufigkeit.put("dreimal", 3);
+		textUndHaeufigkeit.put("auchDreimal", 3);
+		
+	}
+
 }
