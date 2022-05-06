d7779ae27946034e5e48a81f9433a1aa11dbe81f
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-05-06 10:04:22 +0200

    Immutable Lösungshinweise entfernt

--- Grundlagen/src/begriffe/ImmutableTrainer.java
+++ Grundlagen/src/begriffe/ImmutableTrainer.java
@@ -4,23 +4,21 @@ package begriffe;
  * Erzeugt eine Klasse die nach dem Erzeugen nicht mehr ver�ndert werden kann. 
  * Sie hat ein Feld das mutable ist und eins, dass immutable ist.
  */
-public class ImmutableTrainer {					// Declare the class as final so it can not be extended.
+public class ImmutableTrainer {
 
-	public String string;				// Make all fields private so that direct access is not allowed.
-	public StringBuilder stringBuilder;	// Make all fields final so that it's value can be assigned only once.
+	public String string;
+	public StringBuilder stringBuilder;
 
-	public ImmutableTrainer(String string, StringBuilder stringBuilder) {	// Initialize all the fields via a constructor performing deep copy.
+	public ImmutableTrainer(String string, StringBuilder stringBuilder) {
 		this.string = string;
 		this.stringBuilder = stringBuilder;
 	}
-	
-	// Do not provide setter methods for variables ... oder andere Methoden, die den Wert der Variable veraendern
 
 	public String getString() {					
 		return string;
 	}
 
-	public StringBuilder getStringBuilder() {	// Perform cloning of objects in the getter methods to return a copy rather than returning the actual object reference.
+	public StringBuilder getStringBuilder() {
 		return stringBuilder;
 	}
 
