d561896ae5700e26519fcf82a5b11393ce6bedc4
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-07-07 08:46:07 +0200

    Entwurfsmuster Strategie perfektioniert.

--- Entwurfsmuster/src/strategy/Haus.java
+++ Entwurfsmuster/src/strategy/Haus.java
@@ -1,10 +1,14 @@
 package strategy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Predicate;
+
 public class Haus {
 	private Integer marktpreis;
 	private Integer buchwert;
 	private Integer jahresMieteinnahmen;
-	
+
 	public Haus(Integer marktpreis, Integer buchwert, Integer jahresMieteinnahmen) {
 		this.marktpreis = marktpreis;
 		this.buchwert = buchwert;
@@ -22,7 +26,32 @@ public class Haus {
 	public Integer getJahresMieteinnahmen() {
 		return jahresMieteinnahmen;
 	}
-	
-	
+
+	/**
+	 * Verwendet die Bewertungsstragie und gibt dann aus, ob das Haus guenstig oder teuer ist.
+	 */
+	public String istHauspreisGuenstig( ) {
+		// Methodensignatur NICHT aendern!
+		return  "Haus ist guenstig"; // "Haus ist teuer"
+	}
+
+	/**
+	 * Erzeugt ein Haus und bewertet es nach zwei verschiedenen Strategieen: nach dem Buchwert und nach den Mieteinnahmen.
+	 */
+	public static void main(String[] args) {
+
+		// Haus wird mit der Buchwertstrategie geprüft Buchwert > Marktwert
+		Haus einfamilienHaus = new Haus(250_000, 300_000, 20_000);
+		
+		// Haus wird mit der Mieteinnahmenstrategie geprüft Jahresmieteinnahmen * 10 > Marktpreis
+		Predicate<Haus> bewertenNachMieteinnahmenStrategie 
+			= haus -> haus.getJahresMieteinnahmen() * 10 > haus.getMarktpreis();
+		Haus mehrfamilienHaus = new Haus(250_000, 300_000, 20_000);
+
+		// !!! ab hier nichts ändern, auch nicht Methtodensignatur von istHauspreisGuenstig.
+		assertEquals("Haus ist guenstig", einfamilienHaus.istHauspreisGuenstig());
+		assertEquals("Haus ist teuer", mehrfamilienHaus.istHauspreisGuenstig());
+		System.out.println("Erfolgreich beendet.");
+	}	
 
 }
