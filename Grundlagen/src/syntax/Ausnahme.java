881d73ed52a9c01c290c86650b927218ec43f56c
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-07-15 07:42:37 +0200

    Exception in Trainer umgewandelt

--- Grundlagen/src/syntax/Ausnahme.java
+++ Grundlagen/src/syntax/Ausnahme.java
@@ -1,23 +1,15 @@
 package syntax;
 
+
 import javax.management.BadStringOperationException;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Ausnahme {
-
-	public static void main(String[] args) {
-		
-		// Zeigt handeled Exception, mit Teil, der immer aufgerufen wird. 
-		try {
-			handeledExceptionDemo();
-		} catch (BadStringOperationException | NullPointerException exception) {
-			exception.printStackTrace();
-		} finally {
-			System.out.println("\nwird immer aufgerufen");
-		}
-
-		// zeigt unhandeled Exception
-		unhandeledException();
-	}
+	
+	private boolean methodeWurdeAufgerufen;
 
 	/**
 	 * @throws BadStringOperationException
@@ -26,11 +18,51 @@ public class Ausnahme {
 		throw new BadStringOperationException("Falsche String Operation!");
 	}
 
+	@Test
+	@DisplayName("Exception BadStringOperationException soll geworfen werden.")
+	public void test01() {
+		Assertions.assertThrowsExactly( BadStringOperationException.class, () -> handeledExceptionDemo() );
+	}
+
+	/**
+	 * Zeigt handeled Exception, mit Teil, der immer aufgerufen wird.
+	 * Setzt 
+	 * methodeWurdeAufgerufen = true;
+	 */
+	public void ruftHandeledExceptionAuf() {
+		try {
+			handeledExceptionDemo();
+		} catch (BadStringOperationException | NullPointerException exception) {
+		} finally {
+			methodeWurdeAufgerufen = true;
+		}
+	}
+	
+	@Test
+	@DisplayName("Die handeled exception soll richtig aufgerufen werden und die exception soll gefangen werden.")
+	public void test02() {
+		ruftHandeledExceptionAuf();
+		Assertions.assertTrue(methodeWurdeAufgerufen);				
+	}
+
+	@Test
+	@DisplayName("Teil, der bei Exception immer ausgefuehrt wird, soll aufgerufen werden.")
+	public void test03() {
+		ruftHandeledExceptionAuf();
+		Assertions.assertTrue(methodeWurdeAufgerufen);				
+	}	
+	
 	/**
 	 * Wirft eine unbehandelte Ausnahme.
 	 */
 	private static void unhandeledException() {
-		throw new RuntimeException();
+		throw new NullPointerException();
+	}
+	
+	@Test
+	@DisplayName("Unbehandelte Ausnahme soll geworfen werden.") 
+	public void test04() {
+		Assertions.assertThrows(RuntimeException.class, ()->unhandeledException());
 	}
 
 }
