f5649f9cc0ef42ff2fc2e9a73600b55897bc5d38
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-08-01 17:16:19 +0200

    Umbenennen LBS_ZU_KG_FAKTOR

--- TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
+++ TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
@@ -4,7 +4,10 @@ package schiff;
 
 public abstract class ShipSchiffMapper {
 
-	private static final double KG_ZU_lBS_FAKTOR = 2.205;
+	/**
+	 * 1 kg = 2.205 LBS
+	 */
+	private static final double LBS_ZU_KG_FAKTOR = 2.205;
 	
 	public Schiff shipZuSchiff(Ship ship) {
 		return null;
