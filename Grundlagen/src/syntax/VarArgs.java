96bf6ac6553c09b5d713aab929de8069c0ace958
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-09-12 11:06:06 +0200

    Safevarargs perfektioniert und mit Tests abgesichert.

--- Grundlagen/src/syntax/VarArgs.java
+++ Grundlagen/src/syntax/VarArgs.java
@@ -1,39 +1,119 @@
 package syntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://stackoverflow.com/questions/14231037/java-safevarargs-annotation-does-a-standard-or-best-practice-exist
+ *
+ */
 public class VarArgs {
-	
-	/**
-	 * Gebe die als Parameter übergebenen Strings aus.
-	 * @param strings
-	 * beliebig viele Strings
-	 */
 
 	/**
-	 * Gebe die übergebene Parameter aus.
-	 * @param auszugeben
-	 * 	beliebig viele Objekte des parameterisierten Typs
-	 * @return
-	 * gibt die übergebenen Parameter als Array zurück
+	 * Gebe die Anzahl der uebergebenen Strings zurueck.
+	 * Verwende Varargs.
+	 * @param strings beliebig viele Strings
+	 * @return Anzahl der uebergebenen Strings
+	 * 
 	 */
-	
-//	static <T> T[] gebeAusUnSafe( T a, T b ) {
-//
-//		return gebeAusSafe(a,b);
-//
-//	}
+	@SafeVarargs
+	static int gebeAnzahlStringsZurueck( String ... strings ) {	
 
-	public static void main(String[] args) {
+		// Was macht der Compiler aus String ... strings, wenn "Hallo" und "Welt" uebergeben wird? 
 		
-		// beliebig viele Strings augeben
-		
-		
-		// beliebige Objekte ausgeben
-		
-		
-//		String[] stringsUnSafe = gebeAusUnSafe( "a", "b");
-		
-		
-//		System.out.println(stringsUnSafe);		
+		// String[] strings = new String[] { "Hallo", "Welt" };
+
+		return strings.length;
 	}
 
+	@Test
+	@DisplayName("Varargs mit konkretem Typ, hier String")
+	public void teste01() {
+		int anzahlStrings = gebeAnzahlStringsZurueck("eins", "zwei", "drei");
+		assertEquals(3, anzahlStrings);
+	}
+
+	@SafeVarargs
+	static <T> int gebeAnzahlElementZurueck( T ... beliebige ) {
+
+		// Ist diese Methode varargs safe? Warum?
+		
+		// Wenn die Ã¼bergebenen Parameter selber parameterisiert sind, 
+		// kann Java daraus kein Array erzeugen.
+		// Das kann Java also nicht: new T[] { ... }.
+		// Statt dessen macht Java dann:
+		// new Object[] { ... }
+		// Ist aber hier egal, da der Typ der Elemente
+		// des Arrays hier ruhig Object sein kann.
+		
+		return beliebige.length;		
+	}
+	static <T> int demonstriereVarargsSafe( T erster, T zweiter ) {
+		return gebeAnzahlElementZurueck(erster, zweiter);
+	}
+
+	@Test
+	@DisplayName("@SafeVarargs richtig verwendet")
+	public void test02() {
+		int anzahlElemente = demonstriereVarargsSafe("erster", "zweiter");
+		assertEquals(2, anzahlElemente);
+	}
+
+	static <U> U[] gebeAlsArrayZurueckTypUnbekannt( U ... beliebige ) {
+
+		// Was macht der Compiler aus T ... beliebige, wenn T zwar parameterisiert, aber bekannt ist
+		// also z.B. beim Aufruf gebeAlsArrayZurueckTypBekannt( "erster", "zweiter" ) ?
+
+		// Dann macht Java daraus:
+		// new Object[] { ... } wobei Object die kleinste 
+		// gemeinsame Basisklasse der Parameter ist, kÃ¶nnte 
+		// also statt Object auch Fahrzeug sein, wenn das die kleinste
+		// gemeinsame Basisklasse ist.
+		// new Fahrzeug[] { die Fahrzeuge }
+		// In diesem Fall ist alles OK, die Methode macht keine Probleme.
+		
+		// *
+		
+		// Was macht der Compiler aus T ... beliebige, wenn T KEIN konreter Typ ist, 
+		// also die Ã¼bergebenen Parameter selber paramterisiert sind?
+		// Wann wird das zum Problem?
+		
+		// Wenn die Ã¼bergebenen Parameter selber parameterisiert sind, 
+		// kann Java daraus kein Array erzeugen.
+		// Das kann Java also nicht: new T[] { ... }.
+		// Statt dessen macht Java dann:
+		// new Object[] { ... }
+		
+		// Zur Laufzeit kennt Java aber den richtigen Typ. Wenn man dann Object z.B. 
+		// in String umwandeln mÃ¶chte, knallt es.
+
+		return beliebige;
+	}
+
+	static <V> V[] demonstriereNichtVarargsSafe( V erster, V zweiter ) {
+		return gebeAlsArrayZurueckTypUnbekannt(erster, zweiter);
+	}
+	
+	@Test
+	@DisplayName("Wenn die Methode nicht Varargs Safe ist, aber mit konkretem Typ aufgerufen wird, ist es OK")
+	public void test03() {
+		String[] ergebnis = gebeAlsArrayZurueckTypUnbekannt("erster", "zweiter");
+
+		assertEquals("erster", ergebnis[0]);
+		assertEquals("zweiter", ergebnis[1]);
+	}
+
+	@Test
+	@DisplayName("Wenn die Methode nicht Varargs Safe ist und mit parameterisierten Typen aufgerufen wird, soll sie eine Exception werfen")
+	@SuppressWarnings("unused")
+	public void test04() {
+		assertThrows(ClassCastException.class, ()-> {
+			String[] ergebnisString = demonstriereNichtVarargsSafe("erster", "zweiter");}
+		);			
+	}
+	
 }
+
