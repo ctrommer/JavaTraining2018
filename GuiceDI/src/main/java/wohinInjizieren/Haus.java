0499a9956d9a5f503837566d932af96f36ca990b
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-10-16 10:55:16 +0200

    Guice Defaultwert perfektioniert

--- GuiceDI/src/main/java/wohinInjizieren/Haus.java
+++ GuiceDI/src/main/java/wohinInjizieren/Haus.java
@@ -3,6 +3,7 @@ package wohinInjizieren;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.name.Named;
 
 public class Haus {
 	
@@ -13,7 +14,9 @@ public class Haus {
 	
 	private Wand wand;
 	
-	private Dach dach = new Dach("Default Wert von optionalem Dach");
+	private String name = "Defaultwert fuer Name des Hauses: Skyfall";
+	
+	private Integer nummer = 42;
 
 	@Inject
 	public Haus(Fenster fenster) {
@@ -24,15 +27,21 @@ public class Haus {
 	public void setWand(Wand wand) {
 		this.wand = wand;
 	}
-
+	
 	@Inject( optional = true )
-	public void setDach(Dach dach) {
-		this.dach = dach;
+	public void setName( @Named("Hausname") String name ) {
+		this.name = name;		
 	}
 
+	@Inject( optional = true )
+	public void setNummer( @Named("Hausnummer") Integer nummer) {
+		this.nummer = nummer;
+	}
+	
 	@Override
 	public String toString() {
-		return "Haus [tuer=" + tuer + ", fenster=" + fenster + ", wand=" + wand + ", dach=" + dach + "]";
+		return "Haus [tuer=" + tuer + ", fenster=" + fenster + ", wand=" + wand + ", name=" + name + ", nummer="
+				+ nummer + "]";
 	}
 
 	public static Haus erzeugeHausMitGuice() {
--- GuiceDI/src/main/java/wohinInjizieren/HausZusammenbastlerModul.java
+++ GuiceDI/src/main/java/wohinInjizieren/HausZusammenbastlerModul.java
@@ -1,12 +1,13 @@
 package wohinInjizieren;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 public class HausZusammenbastlerModul extends AbstractModule {
 
 	@Override
-	protected void configure() {		
+	protected void configure() {
 		super.configure();
+			bind( String.class).annotatedWith(Names.named("Hausname")).toInstance("Injizierter Wert fuer Name des Hauses");
 	}
-
 }
