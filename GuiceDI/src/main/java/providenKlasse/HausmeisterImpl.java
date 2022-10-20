244c2554535098aea2384df65fc26d02b5f09076
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-10-20 10:03:46 +0200

    providen perfektioniert

--- GuiceDI/src/main/java/providenKlasse/HausmeisterImpl.java
+++ GuiceDI/src/main/java/providenKlasse/HausmeisterImpl.java
@@ -6,7 +6,6 @@ class HausmeisterImpl implements Hausmeister {
 	private String name;
 	private Integer geburtsjahr;
 	
-	@Inject
 	public HausmeisterImpl(String name, Integer geburtsjahr) {
 		this.name = name;
 		this.geburtsjahr = geburtsjahr;
