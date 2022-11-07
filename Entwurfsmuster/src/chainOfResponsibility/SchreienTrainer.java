4e3981c838290c6d8dec7f0b94621d25178b2dd3
ctrom <ctrom@LAPTOP-4F4UMOBG.localdomain>
2022-11-07 14:01:26 +0100

    Test fuer ChainOfResponsibility

---
+++ Entwurfsmuster/src/chainOfResponsibility/SchreienTrainer.java
@@ -0,0 +1,37 @@
+package chainOfResponsibility;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SchreienTrainer {
+
+	/**
+	 * Zeigt chain of Responsibility, indem ein Text zuerst in Grossbuchstaben verwandelt wird 
+	 * und dann ein Ausrufezeichen angefuegt wird.
+	 * 
+	 * z.B.:
+	 * Hallo -> HALLO!
+	 * 
+	 * @param args
+	 */
+	public String verwandleInSchreien( String gesprochen ) {
+
+		Function<String, String> zuGrossBuchstaben = String::toUpperCase;
+		Function<String, String> ausrufezeichenAnfuegen = text -> text + "!";
+
+		Function<String, String> textZuSchreien = zuGrossBuchstaben.andThen(ausrufezeichenAnfuegen);
+
+		return textZuSchreien.apply(gesprochen);
+	}
+
+	@DisplayName("Hallo soll in HALLO! umgewandelt werden.")
+	@Test
+	public void test01() {
+		assertEquals( "HALLO!", verwandleInSchreien("Hallo") );		
+	}
+
+}
