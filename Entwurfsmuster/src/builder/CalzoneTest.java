eb96ae0ba60a033018d75a12572a72a5c9390c7b
ctrom <ctrom@ThinkPad>
2023-08-15 21:27:58 +0200

    imports organisiert

--- Entwurfsmuster/src/builder/CalzoneTest.java
+++ Entwurfsmuster/src/builder/CalzoneTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import builder.Pizza.Belag;
 
 public class CalzoneTest {
 	
