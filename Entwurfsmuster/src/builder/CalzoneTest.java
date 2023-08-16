4e05a8133d895c36a65af631e053eed7a455f170
ctrom <ctrom@ThinkPad>
2023-08-16 15:27:36 +0200

    Tests von Builder perfektioniert

--- Entwurfsmuster/src/builder/CalzoneTest.java
+++ Entwurfsmuster/src/builder/CalzoneTest.java
@@ -1,6 +1,11 @@
 package builder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,21 +15,49 @@ public class CalzoneTest {
 	/**
 	 * <pre>
 	 * Calzone erzeugen mit
-	 * Belag Pilze
+	 * Belag Pilze Zwiebeln
 	 * sosseDrin = true
 	 * </pre>
 	 * @return Calzone speziell für den Test
 	 */
 	public static Calzone erzeugeCalzoneFuerTest() {
-
 		return null;
 	}
-	
+
 	@Test
 	@DisplayName("Kann man die Calzon erzeugen?")
 	public void test01() {
 		Calzone calzone = erzeugeCalzoneFuerTest();
-		assertEquals("Calzone [sosseDrin=true, belaege=[PILZE]]", calzone.toString() );
+		assertEquals("Calzone [sosseDrin=true, toString()=Pizza [belaege=[PILZE, ZWIEBELN]]]", calzone.toString() );
+	}
+	
+	@Test
+	@DisplayName("Sind die Felder von Calzone private und final?" )
+	public void test02() {
+		Field[] fields = Calzone.class.getDeclaredFields();
+		
+		Stream
+			.of(fields)
+			.forEach( field -> {
+				int modifiers = field.getModifiers();
+				assertTrue( Modifier.isFinal(modifiers), field.getName() + " muss final sein." );
+				assertTrue( Modifier.isPrivate(modifiers), field.getName() + " muss private sein.");				
+			});	
+		
+	}
+	
+	@Test
+	@DisplayName("Sind die Felder von Pizza private und final?")
+	public void test03() {
+		Field[] fields = Pizza.class.getDeclaredFields();
+		
+		Stream
+			.of(fields)
+			.forEach( field -> {
+				int modifiers = field.getModifiers();
+				assertTrue( Modifier.isFinal(modifiers), field.getName() + " muss final sein." );
+				assertTrue( Modifier.isPrivate(modifiers), field.getName() + " muss private sein.");				
+			});	
 	}
 
 }
--- Entwurfsmuster/src/builder/FitnessstudioTest.java
+++ Entwurfsmuster/src/builder/FitnessstudioTest.java
@@ -1,6 +1,11 @@
 package builder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,9 +26,23 @@ public class FitnessstudioTest {
 	
 	@Test
 	@DisplayName("Kann man das Fitnesstudio erzeugen?")
-	public void test02() {
+	public void test01() {
 		Fitnessstudio fitnessstudio = erzeugeFitnessstudioFuerTest();
 		assertEquals("Fitnessstudio [trainingsmoeglichkeiten=[FREIHANTEL, KURSE]]", fitnessstudio.toString());
+	}
+	
+	@Test
+	@DisplayName("Sind die Felder von Fitnesstudio private und final?")
+	public void test02() {
+		Field[] fields = Fitnessstudio.class.getDeclaredFields();
+		
+		Stream
+			.of(fields)
+			.forEach( field -> {
+				int modifiers = field.getModifiers();
+				assertTrue( Modifier.isFinal(modifiers), field.getName() + " muss final sein." );
+				assertTrue( Modifier.isPrivate(modifiers), field.getName() + " muss private sein.");				
+			});		
 	}
 
 }
