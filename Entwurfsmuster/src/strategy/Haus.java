336709ae7240c3fd7f21b980f870320b5c4542fb
ctrom <ctrom@ThinkPad>
2023-09-02 10:43:51 +0200

    Entwurfsmuster Strategie mit Unittests

--- Entwurfsmuster/src/strategy/Haus.java
+++ Entwurfsmuster/src/strategy/Haus.java
@@ -1,7 +1,5 @@
 package strategy;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.function.Predicate;
 
 public class Haus {
@@ -9,7 +7,7 @@ public class Haus {
 	private Integer buchwert;
 	private Integer jahresMieteinnahmen;
 
-	public Haus(Integer marktpreis, Integer buchwert, Integer jahresMieteinnahmen) {
+	public Haus( Integer marktpreis, Integer buchwert, Integer jahresMieteinnahmen ) {
 		this.marktpreis = marktpreis;
 		this.buchwert = buchwert;
 		this.jahresMieteinnahmen = jahresMieteinnahmen;
@@ -29,27 +27,13 @@ public class Haus {
 
 	/**
 	 * Verwendet die Bewertungsstragie und gibt dann aus, ob das Haus guenstig oder teuer ist.
+	 *  
+	 * @return Wenn Bewertungsstrategie vorhanden ist "Haus ist guenstig" : "Haus ist teuer", 
+	 * 	sonst "keine Bewertungsstrategie vorhanden"
 	 */
-	public String istHauspreisGuenstig( ) {
-		// Methodensignatur NICHT aendern!
-		return  "Haus ist guenstig"; // "Haus ist teuer"
+	public String istHauspreisGuenstig( ) {		
+			return "keine Bewertungsstrategie vorhanden";
+
+		// "Haus ist guenstig" : "Haus ist teuer";
 	}
-
-	/**
-	 * Erzeugt ein Haus und bewertet es nach zwei verschiedenen Strategieen: nach dem Buchwert und nach den Mieteinnahmen.
-	 */
-	public static void main(String[] args) {
-
-		// Haus wird mit der Buchwertstrategie geprüft Buchwert > Marktwert
-		Haus einfamilienHaus = new Haus(250_000, 300_000, 20_000);
-		
-		// Haus wird mit der Mieteinnahmenstrategie geprüft Jahresmieteinnahmen * 10 > Marktpreis
-		Haus mehrfamilienHaus = new Haus(250_000, 300_000, 20_000);
-
-		// !!! ab hier nichts ändern, auch nicht Methtodensignatur von istHauspreisGuenstig.
-		assertEquals("Haus ist guenstig", einfamilienHaus.istHauspreisGuenstig());
-		assertEquals("Haus ist teuer", mehrfamilienHaus.istHauspreisGuenstig());
-		System.out.println("Erfolgreich beendet.");
-	}	
-
 }
---
+++ Entwurfsmuster/src/strategy/HausTest.java
@@ -0,0 +1,66 @@
+package strategy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HausTest {
+
+	/**
+	 * Haus wird mit der Buchwertstrategie geprüft Buchwert > Marktwert -> guenstig, sonst teuer
+	 * 
+	 * @param zuBewertendesHaus
+	 * @return "Haus ist guenstig" oder "Haus ist teuer"
+	 */
+	public String bewerteMitBuchwertStrategie( Haus zuBewertendesHaus ) {
+		return null;
+	}
+
+	/**
+	 * Haus wird mit der Mieteinnahmenstrategie geprüft Jahresmieteinnahmen * 10 > Marktpreis -> guenstig, sonst teuer
+	 * 
+	 * @param zuBewertendesHaus
+	 * @return "Haus ist guenstig" "oder Haus ist teuer"
+	 */
+	public String bewerteMitMieteinnahmenstrategie( Haus zuBewertendesHaus ) {		
+		return null;
+	}
+
+	@Test
+	@DisplayName("Buchwertstrategie soll guenstig liefern.")
+	public void test01() {
+		Haus haus = new Haus( 200_000, 300_000, 30_000 );
+		assertEquals( "Haus ist guenstig", bewerteMitBuchwertStrategie( haus ) );
+	}
+
+	@Test
+	@DisplayName("Buchwertstrategie soll teuer liefern.")
+	public void test02() {
+		Haus haus = new Haus( 250_000, 200_000, 20_000 ); 
+		assertEquals( "Haus ist teuer", bewerteMitBuchwertStrategie( haus) );
+	}
+
+	@Test
+	@DisplayName("Mieteinnahmenstrategie soll guenstig liefern.")
+	public void test03() {
+		Haus einfamilienHaus = new Haus( 250_000, 300_000, 30_000 );
+		assertEquals( "Haus ist guenstig", bewerteMitMieteinnahmenstrategie( einfamilienHaus ) );
+	}
+
+	@Test
+	@DisplayName("Mieteinnahmenstrategie soll teuer liefern.")
+	public void test04() {		
+		Haus mehrfamilienHaus = new Haus( 250_000, 200_000, 20_000 );
+		assertEquals( "Haus ist teuer", bewerteMitMieteinnahmenstrategie( mehrfamilienHaus) );
+	}
+
+	@Test
+	@DisplayName("Ausgabe, wenn keine Bewertungsstrategie vorhanden.")
+	public void test05() {
+		Haus mehrfamilienHaus = new Haus( 250_000, 200_000, 20_000 );
+		assertEquals( "keine Bewertungsstrategie vorhanden", mehrfamilienHaus.istHauspreisGuenstig() );				
+	}
+}
