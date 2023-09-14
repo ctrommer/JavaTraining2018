2b395d9fb5e9129ad05400174ed46fcefbf3206c
ctrom <ctrom@ThinkPad>
2023-09-14 07:31:35 +0200

    Callback sinnvolleres Beispiel

---
+++ Grundlagen/src/callBack/FormatiererHelfer.java
@@ -0,0 +1,25 @@
+package callBack;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+public class FormatiererHelfer {
+	
+	/**
+	 * Nimmt das Geburtsjahr und ruft damit einen Callback auf, um das Alter formatiert auszugeben.
+	 * 
+	 * @param geburtsJahr
+	 * @param alterFormatierer
+	 * @return das Alter formatiert
+	 */
+	public static String geburtsjahrZuAlterFormatieren( LocalDate geburtsJahr, Function<Integer, String> alterFormatierer ) {
+		
+		LocalDate now = LocalDate.now();
+		Period between = Period.between( geburtsJahr, now );
+		int years = between.getYears();
+		
+		return alterFormatierer.apply(years);		
+	}
+
+}
