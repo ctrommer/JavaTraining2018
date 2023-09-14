24c59b5a432882a911b97c24b0c8c7eae7ef41dd
ctrom <ctrom@ThinkPad>
2023-09-14 07:31:35 +0200

    Callback sinnvolleres Beispiel

---
+++ Grundlagen/src/callBack/FormatiererHelfer.java
@@ -0,0 +1,21 @@
+package callBack;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+public class FormatiererHelfer {
+	
+	/**
+	 * Nimmt das Geburtsjahr und ruft damit einen Callback auf, um das Alter formatiert auszugeben.
+	 * 
+	 * @param geburtsJahr
+	 * @param alterFormatierer
+	 * @return das Alter formatiert
+	 */
+	public static String geburtsjahrZuAlterFormatieren(  ) {
+		
+		return null;		
+	}
+
+}
---
+++ Grundlagen/src/callBack/FormatiererTester.java
@@ -0,0 +1,32 @@
+package callBack;
+
+import java.time.LocalDate;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FormatiererTester {
+	
+	/**
+	 * Demonstriert die Verwendung des Callbacks
+	 * @param geburtsJahr
+	 * @return z.B.: Das Alter betraegt: 25
+	 */
+	public String geburtsjahrZuAlter( LocalDate geburtsJahr ) {
+		// besserer Name fuer callBack: call After
+		// "Das Alter betraegt: "
+		
+		return null;
+	}
+	
+	@Test
+	@DisplayName("Wird das Geburtsjahr in Alter umgewandelt und formatiert?")
+	public void test01() {
+		LocalDate now = LocalDate.now();
+		Assertions.assertEquals("Das Alter betraegt: 25", geburtsjahrZuAlter(now.minusYears(25)));	
+				
+	}
+
+}
