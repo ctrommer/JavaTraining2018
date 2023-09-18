5cbdddacd9becb83525319111593dd4369213640
ctrom <ctrom@ThinkPad>
2023-09-18 13:05:58 +0200

    Test für Enum

---
+++ Grundlagen/src/enumDemo/EnumTester.java
@@ -0,0 +1,53 @@
+package enumDemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EnumTester {
+	
+	private Wochentag erzeugeMontag() {
+		return Wochentag.MONTAG;		
+	}
+
+	private Wochentag erzeugeDienstag() {
+		return Wochentag.DIENSTAG;
+	}
+
+	private Boolean istMontag( Wochentag wochentag ) {
+		return wochentag == Wochentag.MONTAG;
+	}
+
+	private String textVonWochentag( Wochentag wochentag ) {
+		return wochentag.getText();
+	}
+	
+	@DisplayName("Wird Montag erzeugt und hat er den richtigen Text?")
+	@Test
+	public void test01() {
+		Wochentag montag = erzeugeMontag();
+		Assertions.assertEquals( "Montag", textVonWochentag(montag) );		
+	}
+
+	@DisplayName("Wird Dienstag erzeugt und hat er den richtigen Text?")
+	@Test
+	public void test02() {
+		Wochentag dienstag = erzeugeDienstag();
+		Assertions.assertEquals( "Tuesday", textVonWochentag(dienstag) );		
+	}
+	
+	@DisplayName("Funktioniert Prüfung, ob Wochentag ein Montag ist?")
+	@Test
+	public void test03() {
+		Boolean istMontag = istMontag(erzeugeMontag());
+		Assertions.assertTrue(istMontag);		
+	}
+
+	@DisplayName("Funktioniert Prüfung, ob Wochentag ein Dienstag ist?")
+	@Test
+	public void test04() {
+		Boolean istMontag = istMontag(erzeugeDienstag());
+		Assertions.assertFalse(istMontag);		
+	}
+
+}
--- Grundlagen/src/enumDemo/Wochentag.java
+++ Grundlagen/src/enumDemo/Wochentag.java
@@ -1,8 +1,17 @@
 package enumDemo;
 
 /**
- * Wochentage, die man mit Text konstruiert, deren Text man setzen kann und deren Text man abfragen kann.
- * Für Dienstag die Methode überschreiben, die den Text ermittelt.
+ * <pre>
+ * Wochentage, die man 
+ *  mit Text konstruiert, 
+ *  ( "Montag" "Dienstag" "Mittwoch" )
+ *  deren Text man setzen kann und 
+ *  deren Text man abfragen kann.
+ * 
+ * Fuer Dienstag die Methode ueberschreiben, 
+ * die den Text ermittelt. Diese Methode soll 
+ * Tuesday zurueckgeben.
+ * <pre>
  *
  */
 public enum Wochentag {
