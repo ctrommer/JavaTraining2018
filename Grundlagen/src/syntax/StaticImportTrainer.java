6b4aeaf985820151178c4f2a42651d861a535274
ctrom <ctrom@ThinkPad>
2023-09-19 09:23:11 +0200

    Tests fuer Static import und assert

---
+++ Grundlagen/src/syntax/StaticImportTrainer.java
@@ -0,0 +1,20 @@
+package syntax;
+
+import static java.lang.Integer.MAX_VALUE;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StaticImportTrainer {
+	
+	public Integer mitStaticImportIntMaxValue() {
+		return MAX_VALUE;
+	}
+
+	@DisplayName("Wird MAX_VALUE statisch importiert?")
+	@Test
+	public void test01() {
+		Assertions.assertEquals( 2147483647, mitStaticImportIntMaxValue() );
+	}
+}
