2860a2f0682609dff1b6d40ec792395ba92527ee
ctrom <ctrom@ThinkPad>
2023-12-04 11:13:44 +0100

    Perfektioniert: KG_ZU_LBS_FAKTOR

--- TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
+++ TransformGenerator/src/main/java/schiff/ShipSchiffMapper.java
@@ -5,7 +5,7 @@ public abstract class ShipSchiffMapper {
 	/**
 	 * 1 kg = 2.205 LBS
 	 */
-	private static final double LBS_ZU_KG_FAKTOR = 2.205;
+	private static final double KG_ZU_LBS_FAKTOR = 2.205;
 	
 	public Schiff shipZuSchiff(Ship ship) {
 		return null;
@@ -14,4 +14,5 @@ public abstract class ShipSchiffMapper {
 	public Ship schiffZuShip(Schiff schiff) {
 		return null;
 	}
+
 }
