daf5727e470558ee86179775f1deee2066bc91d5
ctrom <ctrom@ThinkPad>
2023-12-23 18:47:21 +0100

    ModulGebrauchtAnderes und ModulWirdGebraucht verhuebscht

--- ModulGebrauchtAnderes/src/modul/gebrauchtAnderes/Main.java
+++ ModulGebrauchtAnderes/src/modul/gebrauchtAnderes/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		IntrinsichenWertBerechner intrinsichenWertBerechner = new IntrinsichenWertBerechner();
-		System.out.println( intrinsichenWertBerechner.berechneWert(10, 20) );
+		System.out.println( intrinsichenWertBerechner.berechneWert( 10, 20 ) );
 	}
 
 }
