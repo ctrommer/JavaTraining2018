b608151eea1c5d0b31b5086383a3e6fd5e7c1093
ctrom <ctrom@ThinkPad>
2024-01-14 20:49:01 +0100

    Bugfix in Master fuer Uebung

--- AufWunsch/src/aufWunschBeispiel/VielleichtMitInhaltTester.java
+++ AufWunsch/src/aufWunschBeispiel/VielleichtMitInhaltTester.java
@@ -1053,7 +1053,8 @@ public class VielleichtMitInhaltTester {
 	@Test
 	@DisplayName("Testet inhaltOderErzeuge mit erzeuger gleich null")
 	public void test50() {
-		Erzeuger<Object> erzeuger = null;
+		@SuppressWarnings("rawtypes")
+		Erzeuger erzeuger = null;
 		
 		// siehe Kommentar zu InvocationTargetException in test03
 		InvocationTargetException exceptionWegenReflektion 
