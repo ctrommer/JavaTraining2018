de8aa0724dd91d1a97870ba77be6044b1d084d48
ctrom <ctrom@ThinkPad>
2024-04-11 21:03:03 +0200

    von Loesung bereinigt

--- Vergleicher/src/training/Angestellter.java
+++ Vergleicher/src/training/Angestellter.java
@@ -1,6 +1,6 @@
 package training;
 
-public class Angestellter implements Comparable<Angestellter> {
+public class Angestellter {
 	Integer gehalt;
 	
 	public Angestellter( Integer gehalt ) {
@@ -12,35 +12,5 @@ public class Angestellter implements Comparable<Angestellter> {
 	public String toString() {
 		return "Angestellter [gehalt=" + gehalt + "]";
 	}	
-	
-	@Override
-	public int compareTo( Angestellter o ) {
-		return gehalt.compareTo( o.gehalt );
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ( ( gehalt == null ) ? 0 : gehalt.hashCode() );
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Angestellter other = (Angestellter) obj;
-		if (gehalt == null) {
-			if (other.gehalt != null)
-				return false;
-		} else if (!gehalt.equals(other.gehalt))
-			return false;
-		return true;
-	}
 	
 }
