61248e67a934b289acc5d7750d240042414ee349
ctrom <ctrom@ThinkPad>
2024-04-21 06:29:56 +0200

    Tests verschoben

--- Grundlagen/src/datumUndZeit/DatumUndZeitTrainer.java
+++ Grundlagen/src/datumUndZeit/DatumUndZeitTrainer.java
@@ -15,35 +15,11 @@ public class DatumUndZeitTrainer {
 		return localDateTime.toString();
 	}
 
-	@Test
-	@DisplayName("Werden aktuelles Datum und aktuelle Uhrzeit richtig umgewandelt?")
-	public void test01() {
-		LocalDateTime vorTest = LocalDateTime.now();
-		
-		String datumUndZeitAlsString = datumUndZeitAlsString();
-		
-		LocalDateTime testErgebnis = LocalDateTime.parse( datumUndZeitAlsString );
-		LocalDateTime nachTest = LocalDateTime.now();
-		Assertions.assertTrue( 
-						vorTest.isBefore( testErgebnis ) );
-		Assertions.assertTrue( 
-						nachTest.isAfter( testErgebnis ) );
-	}
-
 	private String aktuellesDatumInDreiMonaten() {
 		LocalDate localDate = LocalDate
 									.now()
 									.plusMonths( 3 );
 		return localDate.toString();
-	}
-
-	@Test
-	@DisplayName("Wird das aktuelle Datum in drei Monaten richtig ermittelt?")
-	public void test02() {
-		LocalDate testErgebnis = LocalDate.parse( aktuellesDatumInDreiMonaten() );
-		Assertions.assertEquals(
-						LocalDate.now().plusMonths(3).getMonth(), 
-						testErgebnis.getMonth() );
 	}
 
 	/**
@@ -55,7 +31,31 @@ public class DatumUndZeitTrainer {
 		Duration duration = Duration.ofMinutes( 1 );
 		return duration.getSeconds();
 	}
+	
+	@Test
+	@DisplayName("Werden aktuelles Datum und aktuelle Uhrzeit richtig umgewandelt?")
+	public void test01() {
+		LocalDateTime vorTest = LocalDateTime.now();
+		
+		String datumUndZeitAlsString = datumUndZeitAlsString();
+		
+		LocalDateTime testErgebnis = LocalDateTime.parse( datumUndZeitAlsString );
+		LocalDateTime nachTest = LocalDateTime.now();
+		Assertions.assertTrue( 
+				vorTest.isBefore( testErgebnis ) );
+		Assertions.assertTrue( 
+				nachTest.isAfter( testErgebnis ) );
+	}
 
+	@Test
+	@DisplayName("Wird das aktuelle Datum in drei Monaten richtig ermittelt?")
+	public void test02() {
+		LocalDate testErgebnis = LocalDate.parse( aktuellesDatumInDreiMonaten() );
+		Assertions.assertEquals(
+				LocalDate.now().plusMonths(3).getMonth(), 
+				testErgebnis.getMonth() );
+	}
+	
 	@Test
 	@DisplayName("Dauer einer Minute in Sekunden.")
 	public void test03() {
