272da76490ffb3ea8d8d34493f57d16dc1beb701
ctrom <ctrom@ThinkPad>
2024-04-30 20:41:33 +0200

    Tests verschoben Teil2

--- Vergleicher/src/training/ComparatorTrainier.java
+++ Vergleicher/src/training/ComparatorTrainier.java
@@ -88,5 +88,126 @@ public class ComparatorTrainier {
 	private static void sortiereAngestellteNachNatuerlicherOrdnung() {
 		zuSortierendeAngestellte.sort(null);
 	}
+	
+	@Test
+	@DisplayName("Wurde Person nach Name sortiert mit anonymer innerer Klasse?")
+	public void test01() {
+		sortierePersonNachNameMitAnonymerInnererKlasse();
+		
+		assertEquals( 
+				nachNameSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+	
+	@Test
+	@DisplayName("Wurde Person nach Name sortiert mit Lambda?")
+	public void test02() {
+		sortierePersonNachNameMitLambda();
+		
+		assertEquals(
+				nachNameSortiertePersonen, 
+				zuSortierendePersonen );
+	}	
 
+	@Test
+	@DisplayName("Wurde Person nach Name sortiert mit Comparator und Function als Lambda?")
+	public void test03() {
+		sortierePersonNachNameMitLambdaUndFunction();
+		assertEquals(
+				nachNameSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+
+	@Test
+	@DisplayName("Wurde Person nach Name sortiert mit statischer Methode von Comparator?")
+	public void test04() {
+		sortierePersonNachNameMitStatischerMethodeVonComparator();
+		
+		assertEquals(
+				nachNameSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+
+	@Test
+	@DisplayName("Wurde Person nach Name rueckwaerts sortiert?")
+	public void test05() {
+		List<Person> nachNameRueckwaertsSortiertePersonen = Arrays.asList( 
+																	new Person ( "Waltraut", 80 ) , 
+																	new Person ( "Waltraut", 70 ), 
+																	new Person ( "Kevin", 50 ), 
+																	new Person( "KEVIN", 40 ),  
+																	new Person ( "Andreas", 30 ) );
+
+		sortierePersonNachNameRueckwaerts();
+		assertEquals(
+				nachNameRueckwaertsSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+
+	@Test
+	@DisplayName("Wurde Person nach Name rueckwaerts sortiert Gross- Kleinschreibung ignorierend?")
+	public void test06() {
+		List<Person> nachNameRueckwaertsGrossKleinIgnorierendSortiertePersonen = Arrays.asList( 
+																							new Person ( "Waltraut", 80 ) , 
+																							new Person ( "Waltraut", 70 ), 
+																							new Person ("Kevin", 50 ), 
+																							new Person( "KEVIN", 40 ),  
+																							new Person ( "Andreas", 30 ) );
+		
+		sortierePersonNachNameRueckwaertsGrossKleinSchreibungIgnorierend();
+		assertEquals(
+				nachNameRueckwaertsGrossKleinIgnorierendSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+
+	@Test
+	@DisplayName("Wurde Person nach Name Rueckwaerts und dann nach Alter sortiert?")
+	public void test07() {		
+		List<Person> nachNameDannNachAlterSortiertePersonen = Arrays.asList( 
+																		new Person ( "Waltraut", 70 ), 
+																		new Person ( "Waltraut", 80 ), 
+																		new Person ( "Kevin", 50 ), 
+																		new Person( "KEVIN", 40 ), 
+																		new Person ( "Andreas", 30 ) );
+		
+		sortierePersonNachNameRueckwaertsUndDannNachAlter();
+		assertEquals(
+				nachNameDannNachAlterSortiertePersonen, 
+				zuSortierendePersonen );
+	}
+
+	@Test
+	@DisplayName("Wurde Person nach Name mit Nullwerten und dann nach Alter soritert?")
+	public void test08() {
+	
+		List<Person> nachNameUndAlterSortiertePersonenMitNull = Arrays.asList( 
+																		new Person ( "Andreas", 30 ), 
+																		new Person("KEVIN", 40 ), 
+																		new Person ("Kevin", 50 ), 
+																		new Person ( "Waltraut", 70 ), 
+																		new Person ( "Waltraut", 80 ), 
+																		null, 
+																		null );
+
+		sortierePersonNachNameMitNullZuletztUndDannNachAlter();
+
+		assertEquals(
+				nachNameUndAlterSortiertePersonenMitNull, 
+				zuSortierendePersonenMitNull );
+	}
+
+	@Test
+	@DisplayName("Wurden Angestellte nach natuerlicher Ordnung sortiert?")
+	public void test09() {
+		List<Angestellter> nachNatuerlicherOrdnungSortierteAngestellte = Arrays.asList(
+																					new Angestellter(30), 
+																					new Angestellter(40), 
+																					new Angestellter(50), 
+																					new Angestellter(60) );
+		sortiereAngestellteNachNatuerlicherOrdnung();
+		assertEquals(
+				nachNatuerlicherOrdnungSortierteAngestellte, 
+				zuSortierendeAngestellte );
+	}
+	
 }
