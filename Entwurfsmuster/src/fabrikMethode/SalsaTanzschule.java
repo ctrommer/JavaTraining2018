0b63c4da279fef8403fe80777bee78ea0bb16fda
ctrom <ctrom@ThinkPad>
2024-05-09 09:11:28 +0200

    Doku zu Entwurfsmuster Factory

--- Entwurfsmuster/src/fabrikMethode/SalsaTanzschule.java
+++ Entwurfsmuster/src/fabrikMethode/SalsaTanzschule.java
@@ -1,9 +1,9 @@
 package fabrikMethode;
 
-public class SalsaTanzschule extends Tanzschule {
+public class SalsaTanzschule extends Tanzschule {	
 
 	@Override
-	protected Tanzlehrer erzeugeTanzlehrer() {
+	protected Tanzlehrer erzeugeTanzlehrer() {		// 3. Bedingung: Erzeuger-Methode wird überschrieben.
 		return new SalsaTanzlehrer();
 	}
 	
--- Entwurfsmuster/src/fabrikMethode/Tanzschule.java
+++ Entwurfsmuster/src/fabrikMethode/Tanzschule.java
@@ -3,25 +3,15 @@ package fabrikMethode;
 /**
  * Eine Tanzschule verwendet in einem Workshop Tanzlehrer. Eine SalsaTanzschule verwendet SalsaTanzlehrer. 
  * Tanzlehrer und SalsaTanzlehrer sollen mit dem Entwurfsmuster Fabrikmethode erzeugt werden.
- * 
- * - Eine Fabrikmethode ist ... siehe 
- * 		https://refactoring.guru/design-patterns/factory-method
- * 
- * Abgrenzung:
- * 
- * - Wenn eine Klasse eine statische Methode hat, um Instanzen von ihr selbst zu erzeugen, ist das noch keine Fabrikmethode.
- *  
- * 	- Fabrikmethode vs Abstract Factory siehe 
- * 		https://stackoverflow.com/questions/5739611/what-are-the-differences-between-abstract-factory-and-factory-design-patterns?answertab=trending#tab-top  
  *
  */
 public class Tanzschule {
-	protected Tanzlehrer erzeugeTanzlehrer() {
+	protected Tanzlehrer erzeugeTanzlehrer() {		// 1. Bedingung: Methode, die Objekt erzeugt
 		return new Tanzlehrer();
 	}
 
 	public String workshopDurchfuehren() {
-		Tanzlehrer tanzlehrer = erzeugeTanzlehrer();
+		Tanzlehrer tanzlehrer = erzeugeTanzlehrer(); // 2. Bedingung: Erzeuger-Methode wird in der Klasse selbst verwendet 
 		return tanzlehrer.unterrichte();		
 	}
 
