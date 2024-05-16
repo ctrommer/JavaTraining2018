897ae4cc70cd1d910d2b3a3b8a300f9951e76895
ctrom <ctrom@ThinkPad>
2024-05-16 18:26:52 +0200

    Ueberfluessiges Inject entfernt.

--- GuiceDI/src/main/java/providenAnnotation/FoersterImpl.java
+++ GuiceDI/src/main/java/providenAnnotation/FoersterImpl.java
@@ -1,13 +1,10 @@
 package providenAnnotation;
 
-import com.google.inject.Inject;
-
 class FoersterImpl implements Foerster {
 	
 	private String name;
 	private Integer alter;
 	
-	@Inject
 	public FoersterImpl(String name, Integer alter) {
 		this.name = name;
 		this.alter = alter;
