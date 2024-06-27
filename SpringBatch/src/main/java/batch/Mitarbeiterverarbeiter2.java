c89b7782c93f185135d87ded445452e7892a371f
ctrom <ctrom@ThinkPad>
2024-06-27 17:51:36 +0200

    Uebung von Loesung befreit

--- SpringBatch/src/main/java/batch/Mitarbeiterverarbeiter2.java
+++ SpringBatch/src/main/java/batch/Mitarbeiterverarbeiter2.java
@@ -4,24 +4,15 @@ import org.springframework.batch.item.ItemProcessor;
 
 import model.Mitarbeiter;
 
-public class Mitarbeiterverarbeiter2 implements ItemProcessor<Mitarbeiter, Mitarbeiter> {
+public class Mitarbeiterverarbeiter2 {
 	/**
 	 * 1 kg = 2.205 LBS
 	 */
-	private static final double KG_ZU_LBS_FAKTOR = 2.205;
+	
 
 	/**
 	 * Hier ist die Stelle, wo man Business-Logik verwendet. In diesem Fall ist die einzige
 	 * Business Logik die Umrechnung von KG in LBS.
 	 */
-	@Override
-	public Mitarbeiter process( Mitarbeiter mitarbeiter ) throws Exception {
 
-		double gewichtInLBS = mitarbeiter.getGewicht() * KG_ZU_LBS_FAKTOR;
-		mitarbeiter.setGewicht( ( int ) Math.round(gewichtInLBS) );
-		
-		System.out.println( "Processing..." + mitarbeiter );
-
-		return mitarbeiter;
-	}
 }
