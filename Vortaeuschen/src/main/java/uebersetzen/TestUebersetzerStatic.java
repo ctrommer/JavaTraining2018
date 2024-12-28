9829bf0a3a76d8811d8bbb4b9af25efd88c6166d
ctrom <ctrom@ThinkPad>
2024-12-28 19:04:05 +0100

    statische Methode mocken von Loesung befreit

--- Vortaeuschen/src/main/java/uebersetzen/TestUebersetzerStatic.java
+++ Vortaeuschen/src/main/java/uebersetzen/TestUebersetzerStatic.java
@@ -9,20 +9,15 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith( MockitoExtension.class )
 public class TestUebersetzerStatic {
 
 	@Test
 	@DisplayName("statische Methode wird gemockt, schreien soll bei Fuck Beep zurueckgeben.")
 	public void test01() {
-		try ( MockedStatic<Uebersetzer> mockUebersetzer = Mockito.mockStatic( Uebersetzer.class ) ) {
-			mockUebersetzer
-					.when( () -> Uebersetzer.schreien("Fuck") )
-					.thenReturn( "Beep." );
+
 			
 			assertEquals( 
 					"Beep.", 
 					Uebersetzer.schreien("Fuck") );			
-		}
 	}
 }
